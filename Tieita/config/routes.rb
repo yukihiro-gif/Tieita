4ab8e9ca2adc2446a58a45c8280aa98789282759
yukihiro-gif <bsyu14357@gmail.com>
2021-07-09 14:05:54 +0000

    [add]

--- Tieita/config/routes.rb
+++ Tieita/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+  get 'homes/top'
+  get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
