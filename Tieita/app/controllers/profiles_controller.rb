e87405171f03efe0ba4758bf87f13f6c20c28eb8
yukihiro-gif <bsyu14357@gmail.com>
2021-07-15 14:58:08 +0000

    [make]profile/edit

--- Tieita/app/controllers/profiles_controller.rb
+++ Tieita/app/controllers/profiles_controller.rb
@@ -2,25 +2,29 @@ class ProfilesController < ApplicationController
 
   def new
     @user = User.new()
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[])
   end
 
   def create
-    user = User.new(params_user)
-    user.save
-    redirect_to ""
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(params_user)
+    redirect_to ''
   end
 
   def edit
+    @user = User.find(params[:id])
+    @users = User.all
   end
 
 private
  def params_user
-   params.require(:user).permit(:profile_image, :profile_name, :introduction, :tel, :email, :passwrod)
+   params.require(:user).permit(:profile_image, :profile_name, :introduction, :tel, :email, :password)
  end
 end
--- Tieita/config/routes.rb
+++ Tieita/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'profiles/new'
-  resources :pofiles
+  resources :profiles
   devise_for :users
   root to: 'homes#top'
   resources :articles
+  get 'profiles/:id/edit' => 'profiles#edit'
+  get 'articles/:id/show' => 'articles#show'
   get 'homes/top'
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
