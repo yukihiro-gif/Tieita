1cfc9c231cef556e33a39347c7dbe88f1aed7b98
yukihiro-gif <bsyu14357@gmail.com>
2021-07-24 13:13:47 +0000

    [add]

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,6 @@ ActiveRecord::Schema.define(version: 2021_07_22_020707) do
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_likes_on_article_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "post_comments", force: :cascade do |t|
