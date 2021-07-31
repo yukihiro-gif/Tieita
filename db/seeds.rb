5f8a75fee70c9bfb1393776f41a40507190eb51e
yukihiro-gif <bsyu14357@gmail.com>
2021-07-31 13:59:15 +0000

    [add]ranking/slider

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
+ User.create!(
   [{email: 'bsyu00000@gmail.com',
   password: '000000',
   name_sei: '佐藤',
@@ -32,6 +32,7 @@ User.create!(
   name_kana_mei: 'ハナコ',
   tel: '09012345678',
   profile_name: 'ハナ',
+  profile_image: File.open('./app/assets/images/5048383_m.jpg'),
   introduction: 'スポーツと映画鑑賞が趣味です。',
   age: '20',
   job: '花屋',
@@ -89,65 +90,52 @@ User.create!(
   dislike: '虫',},]
 )
 
-Article.create!(
-  [{user_id: '1',
-  title: '美味しいカレーの作り方',
-  situation: 'お家でカレーを作る時に役立ちます',
-  article_image1: File.open('./app/assets/images/5048383_m.jpg'),
-  description1: '野菜を洗う',
-  article_image2: File.open('./app/assets/images/5048383_m.jpg'),
-  description2: '肉にしっかり焼き色をつける',
-  article_image3: File.open('./app/assets/images/5048383_m.jpg'),
-  description3: 'サフランライスを炊く',
-  article_image4: File.open('./app/assets/images/5048383_m.jpg'),
-  description4: 'おしゃれに盛り付けて完成！'},
+ User.all.each do |user|
+ rand(0..5).times do |n|
+    Article.create!([{
+    user_id: user.id,
+    title: 'ユーザ#{user.id}の記事NO.#{n}',
+    situation: '場面#{n}で使います',
+    article_image1: File.open('./app/assets/images/5048383_m.jpg'),
+    description1: 'まずは〇〇を〇〇します。',
+    description2: '次に〇〇を〇〇します。',
+    description3: 'さらには〇〇を〇〇します。',
+    description4: '最後に〇〇を〇〇して完成！',},]
+    )
+  end
+end
 
-  {user_id: '2',
-  title: 'お風呂でカビを取る裏技',
-  situation: 'お風呂に入る前に',
-  article_image1: File.open('./app/assets/images/5048383_m.jpg'),
-  description1: '浴槽を濡らす',
-  article_image2: File.open('./app/assets/images/5048383_m.jpg'),
-  description2: 'たわしで擦る',
-  article_image3: File.open('./app/assets/images/5048383_m.jpg'),
-  description3: 'マジックリンをかける',
-  article_image4: File.open('./app/assets/images/5048383_m.jpg'),
-  description4: '水で流して終わり！'},
+Article.all.each do |article|
+ User.all.each do |user|
+  rand(0..5).times do |n|
+    Favorite.create!(
+      article_id: article.id,
+      user_id: user.id,
+    )
+  end
+ end
+end
 
-  {user_id: '2',
-  title: 'お風呂でカビを取る裏技',
-  situation: 'お風呂に入る前に',
-  article_image1: File.open('./app/assets/images/5048383_m.jpg'),
-  description1: '浴槽を濡らす',
-  article_image2: File.open('./app/assets/images/5048383_m.jpg'),
-  description2: 'たわしで擦る',
-  article_image3: File.open('./app/assets/images/5048383_m.jpg'),
-  description3: 'マジックリンをかける',
-  article_image4: File.open('./app/assets/images/5048383_m.jpg'),
-  description4: '水で流して終わり！'},
+Article.all.each do |article|
+ User.all.each do |user|
+  rand(0..5).times do |n|
+    Like.create!(
+      article_id: article.id,
+      user_id: user.id,
+    )
+  end
+ end
+end
 
-  {user_id: '2',
-  title: 'お風呂でカビを取る裏技',
-  situation: 'お風呂に入る前に',
-  article_image1: File.open('./app/assets/images/5048383_m.jpg'),
-  description1: '浴槽を濡らす',
-  article_image2: File.open('./app/assets/images/5048383_m.jpg'),
-  description2: 'たわしで擦る',
-  article_image3: File.open('./app/assets/images/5048383_m.jpg'),
-  description3: 'マジックリンをかける',
-  article_image4: File.open('./app/assets/images/5048383_m.jpg'),
-  description4: '水で流して終わり！'},]
-)
-PostComment.create!(
-  [{user_id: '2',
-  article_id: '1',
-  comment: 'とても美味しそうですね。母のマル秘レシピも載せておいたので、ぜひ作ってみください。</br>
-  ~〇〇家のマル秘カレーレシピ~</br>
-  ①肉を炒める</br>
-  ②野菜を煮込む</br>
-  ③お皿に盛り付ける</br>'},
+Article.all.each do |article|
+ User.all.each do |user|
+  rand(0..5).times do |n|
+    PostComment.create!(
+      article_id: article.id,
+      user_id: user.id,
+      comment: "ユーザ#{user.id}のコメント",
+    )
+  end
+ end
+end
 
-  {user_id: '1',
-  article_id: '2',
-  comment: 'すごく綺麗になりそうですね！'},]
-)
