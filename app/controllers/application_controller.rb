7004afba2a58c5c5dd12e626e9cc8c5b47e69729
yukihiro-gif <bsyu14357@gmail.com>
2021-07-31 14:48:18 +0000

    [add]layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configue_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configue_permitted_parameters
