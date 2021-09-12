1b32debf96a53e36f33d33ad70880ccb810b1840
yukihiro-gif <bsyu14357@gmail.com>
2021-09-12 14:56:27 +0000

    [alter]css/main-content

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   get 'scores/index'
   post 'scores/create'
   get '/search' => 'searches#search'
   post '/sort' => 'searches#sort'
   resources :profiles
   devise_for :users
-  root to: 'homes#top'
   resources :articles, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
   end
-  get 'homes/top'
+
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'likes/create'
